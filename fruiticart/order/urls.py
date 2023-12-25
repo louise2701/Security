bd47bf87716eebc8a7643fe34fce5514724b7c53
Blxucreep <LoevanLqc@outlook.com>
2023-12-25 06:33:53 +0100

    login, sign in + cookie for the user email

--- fruiticart/order/urls.py
+++ fruiticart/order/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
 
-from .views import home, produces, order_infos, account, contact
+from .views import home, produces, order_infos, account, contact, modify_user_info, contact_submit, login_submit, signin_submit, login, signin, logout
 
 urlpatterns = [
     path('home/', home, name='home'),
+
     path('produces/', produces, name='produces'),
+
     path('order_infos/', order_infos, name='order_infos'),
-    path('account', account, name='account'),
-    path('contact/', contact, name='contact')
+
+    path('account/', account, name='account'),
+    path('account/modify_user_info/', modify_user_info, name='modify_user_info'),
+
+    path('login/', login, name='login'),
+    path('login/login_submit/', login_submit, name='login_submit'),
+
+    path('signin/', signin, name='signin'),
+    path('signin/signin_submit/', signin_submit, name='signin_submit'),
+
+    path('logout/', logout, name='logout'),
+
+    path('contact/', contact, name='contact'),
+    path('contact/contact_submit/', contact_submit, name='contact_submit')
 ]
--- fruiticart/order/views.py
+++ fruiticart/order/views.py
@@ -2,34 +2,64 @@ from django.http import HttpResponse
 from django.shortcuts import render
 #from order.models import fruit, vegetable
 
-'''def list_items():
-    fruits = fruit.objects.all()
-    vegetables = vegetable.objects.all()
-
-    # make a list of fruits and vegetables with their prices that can be send to the html template
-    fruits_list = []
-    vegetables_list = []
-    for f in fruits:
-        fruits_list.append([f.name, f.price])
-    for v in vegetables:
-        vegetables_list.append([v.name, v.price])
-
-    return fruits, vegetables'''
-
 def home(request):
-    return render(request, 'home.html')
+    user_email = request.COOKIES.get('email', None)
+    return render(request, 'home.html', {'user_email': user_email})
 
 def produces(request):
-    # get the list of fruits and vegetables
-    #fruits, vegetables = list_items()
-    # send the list of fruits and vegetables to the html template
-    return render(request, 'produces.html')
+    user_email = request.COOKIES.get('email', None)
+    return render(request, 'produces.html', {'user_email': user_email})
 
 def order_infos(request):
-    return render(request, 'order_infos.html')
+    user_email = request.COOKIES.get('email', None)
+    return render(request, 'order_infos.html', {'user_email': user_email})
 
 def account(request):
-    return render(request, 'account.html')
+    user_email = request.COOKIES.get('email', None)
+    return render(request, 'account.html', {'user_email': user_email})
 
 def contact(request):
-    return render(request, 'contact.html')
+    user_email = request.COOKIES.get('email', None)
+    return render(request, 'contact.html', {'user_email': user_email})
+
+def modify_user_info(request):
+    user_email = request.COOKIES.get('email', None)
+    # do a post request to modify the user info
+    return render(request, 'account.html', {'user_email': user_email})
+
+def contact_submit(request):
+    user_email = request.COOKIES.get('email', None)
+    # do a post request to send the message
+    return render(request, 'contact.html', {'user_email': user_email})
+
+def login(request):
+    user_email = request.COOKIES.get('email', None)
+    return render(request, 'login.html')
+
+def signin(request):
+    user_email = request.COOKIES.get('email', None)
+    return render(request, 'signin.html')
+
+def login_submit(request):
+    # do a get request to login
+    email = 'test@test'
+
+    # if the login is successful
+    response = render(request, 'home.html')
+    response.set_cookie('email', email)
+    return response
+
+def signin_submit(request):
+    # do a post request to signin
+    email = 'test@test'
+
+    # if the login is successful
+    response = render(request, 'home.html')
+    response.set_cookie('email', email)
+    return response
+
+def logout(request):
+    # set the cookie to expire
+    response = render(request, 'home.html')
+    response.delete_cookie('email')
+    return response
