2e63936177014c49094ad9566ddef8511ae92681
Blxucreep <LoevanLqc@outlook.com>
2023-12-26 03:58:17 +0100

    adding common styles + adding function to automatically add products

--- fruiticart/order/urls.py
+++ fruiticart/order/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-from .views import home, produces, order_infos, account, contact, modify_user_info, contact_submit, login_submit, signin_submit, login, signin, logout
+from .views import home, produces, order_infos, confirm_order, login, login_submit, signin, signin_submit, account, modify_user_info, change_password, logout, contact, contact_submit
 
 urlpatterns = [
     path('home/', home, name='home'),
@@ -8,15 +7,17 @@ urlpatterns = [
     path('produces/', produces, name='produces'),
 
     path('order_infos/', order_infos, name='order_infos'),
-
-    path('account/', account, name='account'),
-    path('account/modify_user_info/', modify_user_info, name='modify_user_info'),
+    path('order_infos/confirm_order/', confirm_order, name='confirm_order'),
 
     path('login/', login, name='login'),
     path('login/login_submit/', login_submit, name='login_submit'),
 
     path('signin/', signin, name='signin'),
     path('signin/signin_submit/', signin_submit, name='signin_submit'),
+
+    path('account/', account, name='account'),
+    path('account/modify_user_info/', modify_user_info, name='modify_user_info'),
+    path('account/change_password/', change_password, name='change_password'),
 
     path('logout/', logout, name='logout'),
 
--- fruiticart/order/views.py
+++ fruiticart/order/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-#from order.models import fruit, vegetable
 
 def home(request):
     user_email = request.COOKIES.get('email', None)
@@ -14,30 +13,15 @@ def order_infos(request):
     user_email = request.COOKIES.get('email', None)
     return render(request, 'order_infos.html', {'user_email': user_email})
 
-def account(request):
+def confirm_order(request):
     user_email = request.COOKIES.get('email', None)
-    return render(request, 'account.html', {'user_email': user_email})
-
-def contact(request):
-    user_email = request.COOKIES.get('email', None)
-    return render(request, 'contact.html', {'user_email': user_email})
-
-def modify_user_info(request):
-    user_email = request.COOKIES.get('email', None)
-    # do a post request to modify the user info
-    return render(request, 'account.html', {'user_email': user_email})
-
-def contact_submit(request):
-    user_email = request.COOKIES.get('email', None)
-    # do a post request to send the message
-    return render(request, 'contact.html', {'user_email': user_email})
+    # do a post request to confirm the order
+    return render(request, 'order_infos.html', {'user_email': user_email})
 
 def login(request):
-    user_email = request.COOKIES.get('email', None)
     return render(request, 'login.html')
 
 def signin(request):
-    user_email = request.COOKIES.get('email', None)
     return render(request, 'signin.html')
 
 def login_submit(request):
@@ -45,7 +29,7 @@ def login_submit(request):
     email = 'test@test'
 
     # if the login is successful
-    response = render(request, 'home.html')
+    response = render(request, 'home.html', {'user_email': email})
     response.set_cookie('email', email)
     return response
 
@@ -58,8 +42,31 @@ def signin_submit(request):
     response.set_cookie('email', email)
     return response
 
+def account(request):
+    user_email = request.COOKIES.get('email', None)
+    return render(request, 'account.html', {'user_email': user_email})
+
+def modify_user_info(request):
+    user_email = request.COOKIES.get('email', None)
+    # do a post request to modify the user info
+    return render(request, 'account.html', {'user_email': user_email})
+
+def change_password(request):
+    user_email = request.COOKIES.get('email', None)
+    # do a post request to change the password
+    return render(request, 'account.html', {'user_email': user_email})
+
 def logout(request):
     # set the cookie to expire
     response = render(request, 'home.html')
     response.delete_cookie('email')
     return response
+
+def contact(request):
+    user_email = request.COOKIES.get('email', None)
+    return render(request, 'contact.html', {'user_email': user_email})
+
+def contact_submit(request):
+    user_email = request.COOKIES.get('email', None)
+    # do a post request to send the message
+    return render(request, 'contact.html', {'user_email': user_email})
